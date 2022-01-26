151d61420d176d04c13429f72182959c0c04d07b
Lucas Donha <ludonha@gmail.com>
2022-01-26 17:49:30 -0300

    challenge 1.0

---
+++ script.js
@@ -0,0 +1,47 @@
+var inputCripto = document.querySelector("#btn-cripto");
+
+inputCripto.addEventListener("click", mensagem);
+
+var inputDescripto = document.querySelector("#btn-descripto");
+
+inputDescripto.addEventListener("click", desmensagem);
+
+var caixaMensagem = document.querySelector("#msg");
+
+var texto = document.getElementById("input-texto");
+
+var regex = /^[a-z\s]*$/
+
+function criptografar(texto) {
+    if (regex.test(texto)) {
+        return texto.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    } else {
+        alert("Apenas letras minúsculas e sem acentuação!!!");
+    }
+}
+function descripitografar(texto) {
+    return texto.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+}
+function mensagem(evento) {
+    evento.preventDefault();
+    
+    var textoCofificado = criptografar(texto.value);
+    caixaMensagem.value = textoCofificado;
+    if (caixaMensagem.value === "undefined") {
+        caixaMensagem.value = "Digite novamente."
+    }    
+}
+function desmensagem(evento) {
+    evento.preventDefault();
+    
+    var textoDescodificado = descripitografar(texto.value);
+    caixaMensagem.value = textoDescodificado;
+}
+function copiar() {
+    var copiar = caixaMensagem.value;
+    navigator.clipboard.writeText(copiar);
+    caixaMensagem.value = "";
+}
+
+var botaoCopiar = document.querySelector("#btn-copy");
+botaoCopiar.addEventListener("click", copiar);
